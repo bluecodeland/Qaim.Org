2ad882410dc3bf9ef44673918c2cf9a27b260e5f
bluecodeland <mhtaeb@gmail.com>
2019-02-02 22:05:56 +0330

    asdfadf

--- app/Http/Controllers/dbtestController.php
+++ app/Http/Controllers/dbtestController.php
@@ -1,18 +1,31 @@
 <?php
-
+namespace phplusir\smsir\Controllers;
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
-use Auth;
 
-class dbtestController extends Controller
+use phplusir\smsir\Smsir;
+use phplusir\smsir\SmsirLogs;
+use Auth;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         // $this->middleware('auth');
         $this->middleware(['auth', 'verified']);
     }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
         $users = DB::table('users')->get();
@@ -20,6 +33,8 @@ class dbtestController extends Controller
 
 
         return view('tes', ['users' => $users, 'smses' => $smses->where('id',1)]);
-        
     }
 }
+
+
+
