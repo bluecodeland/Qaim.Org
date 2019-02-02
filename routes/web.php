2b7dbfad833ba200c11acba04b321417157919a9
bluecodeland <mhtaeb@gmail.com>
2019-02-02 21:59:45 +0330

    somefixes

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Auth::routes(['verify' => true]);
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/tes', 'dbtestController@index')->name('tes');
+Route::get('/dbtest', 'dbtestController@index')->name('dbtest');
 Route::get('/chat', function () {
         return view('chat');
     })->middleware('auth');
