3cd0d0b1032e2da7fe0bb7548a3d29256a8c01cc
bluecodeland <mhtaeb@gmail.com>
2019-02-05 19:05:17 +0330

    Temporary fix for app.bale.php
    becuse of email validation, when user registerd and before validating email auth user show error. we should fix it.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
                       @auth
                   <li class="nav-item dropdown">
                     <a class="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="uploads/avatar/{{ $user->avatar }}" alt="{{ $user->name }}" class="avatar">
+                        {{-- <img src="uploads/avatar/{{ $user->avatar }}" alt="{{ $user->name }}" class="avatar"> --}}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                       <a class="dropdown-item" href="{{ url('/home') }}">صفحه شخصی</a>
