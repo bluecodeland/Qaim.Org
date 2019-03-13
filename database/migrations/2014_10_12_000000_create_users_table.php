59a7ba4ea0a0da24f360c03c093cbf6aa6b0141a
bluecodeland <mhtaeb@gmail.com>
2019-03-13 14:28:33 +0330

    old changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('role')->default('normal');     
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('mobile')->unique()->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->default('default.jpg');
-            $table->string('mobile')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
